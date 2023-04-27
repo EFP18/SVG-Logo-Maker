bca54342a9c8353c33b21bdfa280947225cd7655
EFP18 <ester.p18@hotmail.com>
2023-04-27 17:57:46 -0500

    successfully passing tests

--- test/svg.test.js
+++ test/svg.test.js
@@ -11,18 +11,18 @@ describe("SVG", () => {
 
   it("should append text element", () => {
     const expectedSvg =
-      '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><text x="150" y="125" font-size="60" text-anchor="middle" fill="white">A</text></svg>';
+      '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text></svg>';
       const svg = new SVG();
-      // svg.setText("SVG", "blue");
-      // expect(svg.render()).toEqual(expectedSvg);
+      svg.setText("SVG", "white");
+      expect(svg.render()).toEqual(expectedSvg);
 
   });
 
   // An exception test checks for code that should throw an error.
   it("should throw if text exceeds 3 characters", () => {
-    const expectedError = new Error("Text must not exceed 3 characters.");
+    const expectedError = new Error("Text must have up to 3 characters");
     const svg = new SVG();
-    // expect(() => svg.setText("MoreThan3Char", "red")).toThrow(expectedError);
+    expect(() => svg.setText("MoreThan3Char", "red")).toThrow(expectedError);
 
   });
 
